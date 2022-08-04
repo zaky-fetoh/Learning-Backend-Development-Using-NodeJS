f57c29a06125d216544c92d9780e91b92bfb3f28
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-08-04 19:34:37 +0200

    Update schedule-interval.js

--- Day10_NotificationSerWithCronJob/controller/schedule-interval.js
+++ Day10_NotificationSerWithCronJob/controller/schedule-interval.js
@@ -4,15 +4,18 @@ const scheduleOPs = require("./notification-scheduler");
 const schNotification = require("../model/scheduled-notification")
 
 async function serveInterval (I){
+
     const old = interval.getServedInterval()
     interval.setServedInterval(old.end, old.end + I)
     const { start, end } = interval.getServedInterval();
+
     const Notis = await schNotification.find({
         deliveryTime: {
             $gte: start, 
             $lt: end,
         }
     }).sort({deliveryTime: 1,}).exec()
+    
     console.log(`Current Interval from ${new Date(start)} to ${new Date(end)} total Notification ${Notis.length}.`)
     scheduleOPs.chainnedNotification(Notis)
 }
