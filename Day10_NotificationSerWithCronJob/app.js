e667fefa9b6779a8607ba24f6196937ffd1234d0
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-08-04 19:21:50 +0200

    .

--- Day10_NotificationSerWithCronJob/app.js
+++ Day10_NotificationSerWithCronJob/app.js
@@ -7,18 +7,18 @@ const morgan = require("morgan")
 
 
 const PORT = process.env.PORT || 3000
-const INTERVAL = 1
+const INTERVAL = 1 //One Minute Interval
 
 async function run() {
     await mongoose.connect("mongodb://localhost:27017/notifiaction")
 
-    const app = express();
-    app.use(morgan());
-    app.use(express.json());
-    app.use(addNotiRoute);
+    const app = express()
+        .use(morgan())
+        .use(express.json())
+        .use(addNotiRoute);
 
     runSchedule(INTERVAL);
-    
+
     app.listen(PORT, () => {
         console.log(`Server Start listening at port ${PORT}`)
     })
